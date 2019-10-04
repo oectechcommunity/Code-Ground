d3661a92504f6f16282616866cb55b5319e56a40
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2019-10-04 19:00:20 +0530

    Add some solutions in dart

--- dart/Add first and third digit/SmrutiRanjanRana.dart
+++ dart/Add first and third digit/SmrutiRanjanRana.dart
@@ -1,1 +1,9 @@
-import 'dart:io';void main() {      stdout.write("Enter a number : ");        String number = stdin.readLineSync();        print(int.parse(number[0])+int.parse(number[2]));  }
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a number : ");
+
+  String number = stdin.readLineSync();
+
+  print(int.parse(number[0]) + int.parse(number[2]));
+}
