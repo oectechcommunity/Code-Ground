ca746139816e9114877804841ea4020961c9d7e0
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2019-10-04 19:01:43 +0530

    Add some solutions in dart

--- dart/Area of a circle/SmrutiRanjanRana.dart
+++ dart/Area of a circle/SmrutiRanjanRana.dart
@@ -1,1 +1,10 @@
-import 'dart:io';import 'dart:math';void main() {  stdout.write("Put diameter of the circle : ");  int diameter = int.parse(stdin.readLineSync());  print("Area of the circle is ${pi * pow(diameter/2, 2)}");}
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  stdout.write("Put diameter of the circle : ");
+  int diameter = int.parse(stdin.readLineSync());
+  
+
+  print("Area of the circle is ${pi * pow(diameter / 2, 2)}");
+}
--- dart/Area of a square/SmrutiRanjanRana.dart
+++ dart/Area of a square/SmrutiRanjanRana.dart
@@ -1,1 +1,10 @@
-import 'dart:io';import 'dart:math';void main() {  stdout.write("Lenght of a side ? ");    int lengthOfSides = int.parse(stdin.readLineSync());    print("Area of the rectangle is ${pow(lengthOfSides, 2)}");  }
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  stdout.write("Lenght of a side ? ");
+
+  int lengthOfSides = int.parse(stdin.readLineSync());
+
+  print("Area of the rectangle is ${pow(lengthOfSides, 2)}");
+}
