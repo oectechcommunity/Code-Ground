ce564be0eb5547bd84f0c1856309935b7d7862f6
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2019-10-04 18:45:47 +0530

    Add some solutions in dart

---
+++ dart/Area of a square/SmrutiRanjanRana.dart
@@ -0,0 +1,1 @@
+import 'dart:io';import 'dart:math';void main() {  stdout.write("Lenght of a side ? ");    int lengthOfSides = int.parse(stdin.readLineSync());    print("Area of the rectangle is ${pow(lengthOfSides, 2)}");  }
