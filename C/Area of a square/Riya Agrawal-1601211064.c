92016442ae468fa80b89d9fb33f76ab3e750be35
riyaagrawal0698 <56133325+riyaagrawal0698@users.noreply.github.com>
2019-10-05 17:29:49 +0530

    Riya Agrawal-1601211064.c

---
+++ C/Area of a square/Riya Agrawal-1601211064.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ 
+void main() {
+   int side, area;
+ 
+   printf("\nEnter the Length of Side : ");
+   scanf("%d", &side);
+ 
+   area = side * side;
+   printf("\nArea of Square : %d", area);
+}
