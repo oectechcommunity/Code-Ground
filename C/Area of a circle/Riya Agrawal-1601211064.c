16796d2a110be780fc4204ff166425d7ed3d148c
riyaagrawal0698 <56133325+riyaagrawal0698@users.noreply.github.com>
2019-10-12 09:52:23 +0530

    Update Riya Agrawal-1601211064.c

--- C/Area of a circle/Riya Agrawal-1601211064.c
+++ C/Area of a circle/Riya Agrawal-1601211064.c
@@ -7,7 +7,7 @@ void main()
     double dia, area;  
     printf("Enter Diameter of circle\n");  
     scanf("%lf", & dia);  
-    area = fmod((PI*dia*dia),4);  
+    area = (PI*dia*dia)/4;  
     printf("Area of circle : %lf\n", area);  
     getch();    
 }   
