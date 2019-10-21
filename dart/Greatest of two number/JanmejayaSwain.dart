c2ad62842385aa335e34b266610c532118b14c2b
root <root@localhost.localdomain>
2019-10-21 13:13:47 +0530

    Added gretest number in dart

---
+++ dart/Greatest of two number/JanmejayaSwain.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+
+int gratestNumber(int fn, iny sn){
+
+    if(fn > sn){
+
+        return fn;
+    }
+    return sn;
+    }
+
+
+
+
+}
+main(){
+
+stdout.write("enter 1st numnber: ");
+
+int firstNumber =  int.parse(stdin.readLineSync());
+
+stdout.write("enter 2st numnber: ");
+
+int secondNumber =  int.parse(stdin.readLineSync());
+
+print("Your gretest Number is ${gratestNumber(firstNumber, secondNumber)} )
+}
