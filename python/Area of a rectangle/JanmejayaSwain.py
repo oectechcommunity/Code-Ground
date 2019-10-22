cc9f69244d3af2d3edddffb9fa948f7339b4d42b
root <root@localhost.localdomain>
2019-10-22 15:01:54 +0530

    Added Area of a Rectangle in python

---
+++ python/Area of a rectangle/JanmejayaSwain.py
@@ -0,0 +1,8 @@
+
+length = float(input('Please Enter the Length of a Rectangle: '))
+width = float(input('Please Enter the Width of a Rectangle: '))
+
+# calculate the area
+area = length * width
+
+print("The Area of a Rectangle using", length, "and", width, " = ", area)
