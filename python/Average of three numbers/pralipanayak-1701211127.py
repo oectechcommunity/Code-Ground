97d5cb507ad10e91b2bf2b4d1c8d668a2a8e1d99
unknown <pralipa.nayal99@gmail.com>
2020-10-02 21:27:11 +0530

    armstrong number solution

--- python/Average of three numbers/pralipanayak-1701211127.py
+++
@@ -1,6 +0,0 @@
-n1=int(input("enter 1st number:"))
-n2=int(input("enter 2nd number:"))
-n3=int(input("enter 3rd number:"))
-total=n1+n2+n3
-avg=total/3
-print("average is:",avg)
--- python/Fibonacci series/pralipanayak-1701211127.py
+++
@@ -1,9 +0,0 @@
-range1=int(input("Enter how many numbers you want in the fibonacci series:"))
-a=0
-b=1
-for i in range(range1):
-    print(a)
-    c=a+b
-    a=b
-    b=c
-print(b)
