b4a3f191977b69953da781401e32871030e204f7
Anuska Lenka <anuska98@gmail.com>
2020-10-04 17:05:19 +0530

    anuskalenka-1821211011

---
+++ python/Average of three numbers/anuskalenka-1821211011.py
@@ -0,0 +1,9 @@
+ #Write a program to find out average of three numbers.
+#Input : 3 numbers
+#Output : Average
+n1 = int(input("enter first number"))
+n2 = int(input("enter second number"))
+n3 = int(input("enter third number"))
+total = n1+n2+n3
+avg = total/3
+print("avg is",avg)
