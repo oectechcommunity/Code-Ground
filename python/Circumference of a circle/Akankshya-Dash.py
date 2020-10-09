1d446890e98d96ea4441f15398f9b75a012ad4be
Akankshya-Dash <akankshyadash2016@gmail.com>
2020-10-09 14:47:40 +0530

    added circumference of circle

---
+++ python/Circumference of a circle/Akankshya-Dash.py
@@ -0,0 +1,14 @@
+# Python3 code to find 
+# circumference of circle 
+
+PI = 3.1415
+
+# utility function 
+def circumference(r): 
+	return (2 * PI * r) 
+
+
+# driver function 
+print ('%.3f' % circumference(5)) 
+
+
