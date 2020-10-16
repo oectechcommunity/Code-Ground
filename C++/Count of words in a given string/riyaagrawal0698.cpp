06a1db9da38445f7ec819d363f903cdd5e81b7c0
RiyaAgrawal <riyaagrawal0698@outlook.com>
2020-10-17 01:31:54 +0530

    riyaagrawal0698

--- C++/Count of words in a given string/riyaagrawal0698.cpp
+++ C++/Count of words in a given string/riyaagrawal0698.cpp
@@ -1,1 +1,18 @@
-Hello Hello
+#include<iostream>
+#include<string.h>
+#include<cstdio>
+using namespace std;
+int main ()
+{
+    char str[100];
+    int count = 0, i; 
+    cout << "Enter a string : ";
+    gets(str);
+    for (i = 0; str[i] != '\0'|| str[i] == '\n' || str[i] == '\t';i++)
+    {
+        if (str[i] == ' ')
+            count++;    
+    }
+    cout << "Number of words in the string are: " << count + 1;
+    return 0;
+}
