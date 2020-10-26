66f7c538e24d603b558390e86d30801f90208529
laxminarayan1998 <laxminarayan1998@icloud.com>
2020-10-26 11:57:00 +0530

    laxminarayan1998.java

---
+++ java/Length of string/laxminarayan1998.java
@@ -0,0 +1,6 @@
+class CalcLength {
+    public static void main( String args[] ) {
+       String name = "oectechcommunity";  
+       int length = name.length(); 
+       System.out.println("The length of the String \""+name+"\" is: " +length);    }
+}
