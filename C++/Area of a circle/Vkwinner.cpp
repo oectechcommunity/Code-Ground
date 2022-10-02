983525a0341294e437d95a7bf0b2961d02d7b945
Vkwinner <72137312+Vkwinner@users.noreply.github.com>
2022-10-02 17:11:02 +0530

    Create Vkwinner.cpp

---
+++ C++/Area of a circle/Vkwinner.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int radius;
+  cout<<"Enter radius"<<endl;
+  cin>>radius;
+  float area=3.14*radius*radius;
+  cout<<"Area of the circle is : " <<area<<endl;
+  return 0;
+}
