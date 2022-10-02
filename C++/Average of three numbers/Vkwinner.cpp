f4ab2fd658ee18ea3bb485fb5eb69b14e2c92747
Vkwinner <72137312+Vkwinner@users.noreply.github.com>
2022-10-02 17:32:12 +0530

    Create Vkwinner.cpp

---
+++ C++/Average of three numbers/Vkwinner.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int num1,num2,num3;
+  float avg;
+  cout<<"Enter 3 Numbers : "<<endl;
+  cin>>num1>>num2>>num3;
+  avg=float(num1+num2+num3)/3;
+  cout<<"Average of 3 Numbers is : "<<avg;
+  
+  return 0;
+}
